2cf01d7058c64b9d06cc71070475f400d903e047
Paul Debevec <paulmdebevec@gmail.com>
2019-12-19 10:31:01 -0700

    Added testing for finding the average price of snacks in machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,12 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  # def average_price
+  #   require "pry"; binding.pry
+  #     snacks.each do |snack|
+  #     puts price = snack.price.delete("$").to_f
+  #     puts average(price)
+  #   end
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+sam = Owner.create(name: "Sam's Snacks")
+machine_1 = sam.machines.create(location: "Don's Mixed Drinks")
+machine_2 = sam.machines.create(location: "Turing Basement")
+snack_1 = machine_1.snacks.create!(name: "twizzlers", price: "$1.50")
+snack_2 = machine_1.snacks.create!(name: "M&M's", price: "$1.00")
+snack_3 = machine_2.snacks.create!(name: "Donuts", price: "$1.50")
+snack_4 = machine_2.snacks.create!(name: "Poptarts", price: "$1.75")
